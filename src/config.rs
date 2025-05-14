e2bb0d5177bc8cf0ef3ab77c2ab5b291bcd419ee
lan-ps <3800cm@gmail.com>
2025-05-14 16:15:20 +0800

    更新

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.808888.best"];
+pub const RS_PUB_KEY: &str = "jzSBafTgrlnn8q5eLbh8Lug0iJlKLwDjZs6RO1W8vLQ=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
